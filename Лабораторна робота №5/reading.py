ce2542d612efb5b90a27ab0d7e5daa275aa29af9
Dmitriy Truten <D.Truten.FIT.122.20@knute.edu.ua>
2020-10-28 17:16:12 +0200

    rename

--- Лабораторна робота №5/reading.py
+++ Лабораторна робота №5/reading.py
@@ -2,3 +2,7 @@ import csv
 import sys
 filename = "the_first_group.txt" 
 fd = open("the_first_group.txt", "r")
+reader = csv.reader(fd, delimiter="\t")
+for row in reader:
+    print(row)
+fd.close() 
