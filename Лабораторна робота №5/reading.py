d862196044dbd834d160aed4730db0fdfab4fbf6
Dmitriy Truten <D.Truten.FIT.122.20@knute.edu.ua>
2020-10-29 14:54:48 +0200

    q

--- Лабораторна робота №5/reading.py
+++ Лабораторна робота №5/reading.py
@@ -5,3 +5,8 @@ fd = open(filename, "r")
 reader = csv.reader(fd, delimiter="\t")
 for row in reader:     
     print(row)
+fd.close()
+fd = open(filename, "a+")
+x=input(1 or 2)
+if(x==1):
+    print("Ok")
