bf04739b0072d23ce4f3fe7768d7588a515cc7e9
Dmitriy Truten <D.Truten.FIT.122.20@knute.edu.ua>
2020-10-29 16:04:43 +0200

    q

--- Лабораторна робота №5/reading.py
+++ Лабораторна робота №5/reading.py
@@ -1,9 +1,12 @@
 import csv 
-import sys 
+import sys
+import glob  
+import os
 filename = "the_first_group.txt"
 fd = open(filename, "r")
 reader = csv.reader(fd, delimiter="\t")
 for row in reader:     
     print(row)
 fd.close()
-fd = open(filename, "a+")
+for filename in os.listdir("D:\-ICS-18092020\Лабораторна робота №5\the_first_group\\plugins"):    
+    print(filename)
