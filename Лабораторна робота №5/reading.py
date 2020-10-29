e8400b2bd7e0ab8b9b0d7b966321e26616e11f8f
Dmitriy Truten <D.Truten.FIT.122.20@knute.edu.ua>
2020-10-29 16:07:12 +0200

    q

--- Лабораторна робота №5/reading.py
+++ Лабораторна робота №5/reading.py
@@ -8,5 +8,3 @@ reader = csv.reader(fd, delimiter="\t")
 for row in reader:     
     print(row)
 fd.close()
-for filename in os.listdir("D:\-ICS-18092020\Лабораторна робота №5\the_first_group\\plugins"):    
-    print(filename)
