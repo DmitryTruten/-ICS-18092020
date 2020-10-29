09f1ed32443cc699f8e062346a1e16a545834650
Dmitriy Truten <D.Truten.FIT.122.20@knute.edu.ua>
2020-10-29 14:39:09 +0200

    q

--- Лабораторна робота №5/reading.py
+++ Лабораторна робота №5/reading.py
@@ -1,7 +1,7 @@
-import csv
-import sys
-filename = "the_first_group.txt" 
-fd = open("the_first_group.txt", "r")
+import csv 
+import sys 
+filename = "the_first_group.txt"
+fd = open(filename, "r")
 reader = csv.reader(fd, delimiter="\t")
-for row in reader:
+for row in reader:     
     print(row)
