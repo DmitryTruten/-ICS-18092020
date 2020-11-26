a994b1ba43e9827ab57b918bce9e66c2b0271855
Dmitriy Truten <D.Truten.FIT.122.20@knute.edu.ua>
2020-11-26 17:27:23 +0200

    Function

--- Лабораторна робота №7/Function.py
+++ Лабораторна робота №7/Function.py
@@ -1,9 +1,9 @@
 from numpy import *
 import matplotlib.pyplot as plt
-def f(t): 
+def f(t):
     return t== 5*sin(10*t)*sin(3*t)
-t = linspace(0, 8, 20)
-y = f(t)
-
+t=linspace(0, 8, 20)
+y=f(t)
+ 
 plt.plot(t,y)
 plt.show()
